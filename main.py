dae6d866147543d2f0fd83722bee4867a280e443
Chiawei Chien <142188887+memefather@users.noreply.github.com>
2023-09-21 15:32:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from langchain.agents.agent_types import AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import tabulate
 import matplotlib
-from st_audiorec import st_audiorec
+from audio_recorder_streamlit import audio_recorder
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
