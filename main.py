a06a15e9de3b90ccfcac108c62e995eaeceec1a1
Chiawei Chien <142188887+memefather@users.noreply.github.com>
2023-09-22 08:24:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def autoplay_audio(file_path: str):
         b64 = base64.b64encode(data).decode()
         md = f"""
             <audio controls autoplay="true">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/wav">
             </audio>
             """
         st.markdown(
